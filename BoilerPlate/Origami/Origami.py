d607fe6e921f673e6a8bf06329d3ac3a83173df7
eupham <evan.upham@outlook.com>
2023-04-09 14:40:14 -0400

    Made a square using mayavi on a spherical prjtn

--- BoilerPlate/Origami/Origami.py
+++ BoilerPlate/Origami/Origami.py
@@ -1,52 +1,27 @@
-#This does not make a square or origami yet
+#This is a square
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
-from mpl_toolkits.mplot3d import Axes3D
+from mayavi import mlab
 
-# Define the spherical coordinates for the four vertices of the square
-r = 1.0
-phi = np.pi/2
-theta = np.linspace(np.pi/4, 3*np.pi/4, 4)
-vertices = np.zeros((4, 3))
-vertices[:, 0] = r * np.sin(theta) * np.cos(phi)
-vertices[:, 1] = r * np.sin(theta) * np.sin(phi)
-vertices[:, 2] = r * np.cos(theta)
+# Define the radius and the angles for the four points
+r = 1
+theta_values = np.array([np.pi / 4, np.pi / 4, 3 * np.pi / 4, 3 * np.pi / 4])
+phi_values = np.array([0, np.pi / 2, np.pi / 2, 0])
 
+# Define the points A, B, C, and D in the r, theta, and phi space
+points_r = np.array([r, r, r, r])
+points_theta = theta_values
+points_phi = phi_values
 
-# Define a color to fill the square
-color = 'b'
+# Plot the points A, B, C, and D in the r, theta, and phi space using Mayavi
+fig = mlab.figure()
+mlab.points3d(points_r, points_theta, points_phi, scale_mode='none', scale_factor=0.1)
 
-# Create the Poly3DCollection object
-square = Poly3DCollection([vertices], alpha=0.25, facecolor=color)
+# Connect the points with lines
+for i in range(4):
+    lines_r = np.array([points_r[i], points_r[(i + 1) % 4]])
+    lines_theta = np.array([points_theta[i], points_theta[(i + 1) % 4]])
+    lines_phi = np.array([points_phi[i], points_phi[(i + 1) % 4]])
+    mlab.plot3d(lines_r, lines_theta, lines_phi, tube_radius=0.01)
 
-# Plot the square
-fig = plt.figure(figsize=(8,8)) # increase size of figure
-ax = fig.add_subplot(111, projection='3d')
-ax.add_collection(square)
-
-# Add labels to the vertices
-for i, vertex in enumerate(vertices):
-    ax.text(vertex[0], vertex[1], vertex[2], f'P{i+1}', ha='center', va='center', color='k')
-
-# Use spherical markers for the vertices
-ax.scatter(vertices[:, 0], vertices[:, 1], vertices[:, 2], c='k', marker='o')
-
-# Set limits and labels
-ax.set_xlim([-1.1, 1.1])
-ax.set_ylim([-1.1, 1.1])
-ax.set_zlim([-1.1, 1.1])
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-
-# Add a table showing the addresses of the vertices
-rows = ['Vertex', 'X', 'Y', 'Z']
-data = [[f'P{i+1}'] + list(vertices[i]) for i in range(4)]
-table = ax.table(cellText=data, colLabels=rows, loc='top')
-table.auto_set_column_width(col=list(range(4)))
-table.auto_set_font_size(True)
-
-fig.subplots_adjust(top=0.8) # adjust plot margins
-
-plt.show()
+# Display the visualization
+mlab.show()
