3cef1e0ee7238840810505f1ccebb864575eb81e
eupham <evan.upham@outlook.com>
2023-04-14 12:07:47 -0400

    Various projects

--- WikiSummarizer/summarizer.py
+++ WikiSummarizer/summarizer.py
@@ -1,3 +1,13 @@
+#Citing the current model
+# @misc {peter_szemraj_2022,
+#     author       = { {Peter Szemraj} },
+#     title        = { long-t5-tglobal-base-16384-book-summary (Revision 4b12bce) },
+#     year         = 2022,
+#     url          = { https://huggingface.co/pszemraj/long-t5-tglobal-base-16384-book-summary },
+#     doi          = { 10.57967/hf/0100 },
+#     publisher    = { Hugging Face }
+# }
+
 import torch
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
 
@@ -13,12 +23,11 @@ summarizer = pipeline(
     device=0 if torch.cuda.is_available() else -1,
 )
 
-def summarize(text, max_length=100):
+def summarize_text(text, max_length=100):
     result = summarizer(text, max_length=max_length, min_length=30, do_sample=False, num_beams=4)
     summary = result[0]["summary_text"]
     return summary
 
-# Example usage:
-web_scrape = "Your web scrape text here..."
-summary = summarize(web_scrape)
+input_text = """Your input text here..."""
+summary = summarize_text(input_text)
 print(summary)
