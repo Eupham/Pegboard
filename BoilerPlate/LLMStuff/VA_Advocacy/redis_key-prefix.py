5e8dbfe2c215c18dcb0a0768222494c54c6e6aea
eupham <evan.upham@outlook.com>
2023-04-17 21:22:56 -0400

    added remaining key-prefixes to vaa

--- BoilerPlate/LLMStuff/VA_Advocacy/redis_key-prefix.py
+++ BoilerPlate/LLMStuff/VA_Advocacy/redis_key-prefix.py
@@ -1,14 +1,16 @@
+#redis_key-prefix.py
 import redis
 from typing import Dict
 
 # Create connections to Redis instances (assuming you have them configured)
 client = redis.Redis('localhost', 6379)
-event_bus = redis.Redis('localhost', 6379)
 
 def create_key_prefixes():
     client.setnx("Party_Module", "partymodule")
-    event_bus.setnx("Event_Module", "eventmodule")
-    # Add more key prefix creation lines below
+    client.setnx("Entity_Module", "entitymodule")
+    client.setnx("Event_Module", "eventmodule")
+    client.setnx("Nash_Module", "nashmodule")
+    client.setnx("Integration_Module", "ntegrationmodule")
 
 if __name__ == '__main__':
     create_key_prefixes()
