6252697d1312c5d7589e25b871fe537e165e6cc3
eupham <evan.upham@outlook.com>
2023-04-19 12:19:57 -0400

    revised properties, attributes, methods in VAA

--- BoilerPlate/LLMStuff/VA_Advocacy/redis_keys.py
+++ BoilerPlate/LLMStuff/VA_Advocacy/redis_keys.py
@@ -4,31 +4,36 @@ client = redis.Redis(host='localhost', port=6379, db=0)
 
 MODULES = {
     "party": {
-        "attributes": ["name", "location", "date"],
-        "metadata": ["id", "description"]
+        "properties": ["name", "date_formed", "date_disbanded"],
+        "attributes": ["type", "entity_set","event_set", "objective_set", "action_set"],
+        "metadata": ["id", "description", ""]
     },
     "entity": {
-        "attributes": ["name", "interests", "contact_hashes"],
-        "metadata": ["id", "results"]
+        "properties": ["name", "date_initiated", "date_inactive"],
+        "attributes": ["type", "objective_set", "party_set", "party_role_set"],
+        "metadata": ["id", "description", "contact_hashes"]
     },
     "event": {
-        "attributes": ["name", "location", "date"],
+        "properties": ["name", "location", "datetime_start", "datetime_end"],
+        "attributes": ["type", "party_set", "result_set"],
         "metadata": ["id", "description"]
     },
     "nash": {
-        "attributes": ["name", "location", "date"],
-        "metadata": ["id", "description"]
-    },
-    "integration": {
-        "attributes": ["name", "location", "date"],
-        "metadata": ["id", "description"]
+        "properties": ["name"],
+        "attributes": ["event_set", "interest_weight_set","payoff_set"],
+        "metadata": ["id"]
     }
 }
 
 def create_all_module_objects():
     for module_name, module_data in MODULES.items():
+        create_module_properties(module_name, module_data["properties"])
         create_module_attributes(module_name, module_data["attributes"])
         create_module_metadata(module_name, module_data["metadata"])
+
+def create_module_properties(module_name, property):
+    for property in properties:
+        client.zadd(f"{module_name}:property:{property}", {"": 0})
 
 def create_module_attributes(module_name, attributes):
     for attribute in attributes:
