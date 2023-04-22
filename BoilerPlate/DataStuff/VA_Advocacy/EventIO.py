7da76401e594f32626d3a1e18be1e93f41e1fa0c
eupham <evan.upham@outlook.com>
2023-04-22 15:29:11 -0400

    Further progress on vaa

--- BoilerPlate/DataStuff/VA_Advocacy/EventIO.py
+++ BoilerPlate/DataStuff/VA_Advocacy/EventIO.py
@@ -32,7 +32,7 @@ class EventIO:
         self.event_module.client.zremrangebyscore(f"{self.event_module.name}:metadata:description", event_id, event_id)
 
 if __name__ == '__main__':
-    event_io = EventIO()
+    event_io = EventIO
 
     # Add an event
     event_io.add_event(1, "New Year's Party", "123 Main St", "2023-12-31T20:00:00", "2024-01-01T02:00:00", "Party", "1,2", "1,2,3", "A fun party to ring in the new year!")
